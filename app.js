2eb0214bf829e4b5607022f94e980c084adbfadc
islomummati8 <islomummati8@gmail.com>
2026-08-31 15:19:51 +0600

    aaa

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 let currentCity = "Tashkent";
 let currentCountry = "Uzbekistan";
-
 async function initPrayerTimes() {
   const citySelect = document.getElementById('citySelect');
   if (citySelect) {
